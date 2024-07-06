36c1d2fb550021a04d63e8c38f41dd598893ea20
ChetSovisoth <chet.sovisoth@gmail.com>
2024-07-06 12:45:19 +0700

    Verify Middleware

---
+++ app/Http/Middleware/VerifyMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if($request->user()->email_verified_at === null) {
+            return redirect()->route('verification.notice.send')->with('danger','Please Verify Email First!');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CVController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\SaveJobController;
+use App\Http\Middleware\VerifyMiddleware;
 use App\Models\SavedJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,12 +12,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/email/verify', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return view('auth.verify');
 });
+
+Route::get('/email/verify/user', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice.send');
 
 Route::get('/profile/create', function () {
     return view('profile.profile_create');
@@ -36,12 +42,16 @@ Route::delete('/profile/delete', [CVController::class,'destroy'])->name('cv.dest
 
 Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
 
-Route::get('/save/job', [SaveJobController::class,'index'])->name('save.index');
+Route::group([
+    'middleware' => VerifyMiddleware::class
+], function () {
 
-Route::post('/save/job/{job}', [JobController::class,'save'])->name('save.job');
+    Route::get('/save/job', [SaveJobController::class,'index'])->name('save.index');
 
-Route::post('/unsave/job/{job}', [JobController::class,'unsave'])->name('unsave.job');
+    Route::post('/save/job/{job}', [JobController::class,'save'])->name('save.job');
 
-Route::post('/unsaved/job/{job}', [SaveJobController::class,'unsave'])->name('unsaved.job');
+    Route::post('/unsave/job/{job}', [JobController::class,'unsave'])->name('unsave.job');
 
+    Route::post('/unsaved/job/{job}', [SaveJobController::class,'unsave'])->name('unsaved.job');
+});
 Route::post('/cv/save', [CVController::class,'store'])->name('cv.store');
